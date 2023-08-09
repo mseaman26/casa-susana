1d535a00fc5051b2e1d3e448f7f9aed10c7f6ee6
michael seaman <mseaman26@gmail.com>
2023-08-09 08:53:34 -1000

    scroll buttons on menu page

--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import menuData from '../../assets/menu.json'
 import MenuItem from "../../components/MenuItem/MenuItem";
 import './Menu.css'
+import { animateScroll as scroll, scroller } from "react-scroll";
 
 const Menu = function(){
 
@@ -14,19 +15,40 @@ const Menu = function(){
          groupedMenuData[section].push(menuItem);
      });
 
+    const scrollToSection = (section) => {
+        scroller.scrollTo(section, {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart',
+            offset: -120
+        });
+    };
+
     return(
         <>
         <div className="menu_page_container">
-        <h1>Menu</h1>
-        
-            {Object.keys(groupedMenuData).map(section => (
-                <div key={section} className="menu_section">
-                    <h2>{section}</h2>
-                    {groupedMenuData[section].map((menuItem, index) => (
-                        <MenuItem menuItem={menuItem} key={index} />
-                    ))}
+            <div className="menu_header">
+                <h1>Menu</h1>
+                <div>
+                    <button onClick={() => scrollToSection('Appetizers')}>Appetizers</button>
+                    <button onClick={() => scrollToSection('Small Plates')}>Small Plates</button>
+                    <button onClick={() => scrollToSection('Combinations')}>Combinations</button>
+                    <button onClick={() => scrollToSection('Main Courses')}>Main Courses</button>
+                    <button onClick={() => scrollToSection('Sides')}>Sides</button>
+                    <button onClick={() => scrollToSection('Desserts')}>desserts</button>
+                    <button onClick={() => scrollToSection('Beverages')}>Beverages</button>
                 </div>
-            ))}
+            </div>
+            <div className="menu_items_container">
+                {Object.keys(groupedMenuData).map(section => (
+                    <div key={section} className="menu_section" id={section}>
+                        <h2>{section}</h2>
+                        {groupedMenuData[section].map((menuItem, index) => (
+                            <MenuItem menuItem={menuItem} key={index} />
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
         </>
     )
