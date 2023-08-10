25a4cad757813a2bc4ddf79919efb9fc505fbd27
michael seaman <mseaman26@gmail.com>
2023-08-10 13:46:39 -1000

    cleared up a couple warnings

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
     return(
         <>
         <nav className='fixed_top'>
-            <NavLink exact to="/" activeClassName="active">Home</NavLink>
-            <NavLink to="/menu" activeClassName="active">Menu</NavLink>
-            <NavLink to="/order" activeClassName="active">Order Online</NavLink>
-            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+            <NavLink to="/" activeclassname="active">Home</NavLink>
+            <NavLink to="/menu" activeclassname="active">Menu</NavLink>
+            <NavLink to="/order" activeclassname="active">Order Online</NavLink>
+            <NavLink to="/contact" activeclassname="active">Contact</NavLink>
         </nav>  
         </>
     )
