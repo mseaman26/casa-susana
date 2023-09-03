ef24caa24f072bc250db040657bf68278285ab72
michael seaman <mseaman26@gmail.com>
2023-09-03 12:51:00 -0700

    getting item form closer to functional

--- client/src/components/Order-Item/OrderItem.jsx
+++ client/src/components/Order-Item/OrderItem.jsx
@@ -6,6 +6,8 @@ const OrderItem = function({ menuItem, order, setOrder, index}){
     const [itemFormShown, setItemFormShown] = useState(false)
     const [itemFormPosition, setItemFormPosition] = useState({top: 0, left: 0})
     const [itemFormXPosition, setItemFormXPosition] = useState(0)
+    const [itemQuantity, setItemQuantity] = useState(1)
+    const [itemTotal, setItemTotal] = useState(0)
 
     const addItem = () => {
         if(!order[menuItem.name]){
@@ -19,6 +21,20 @@ const OrderItem = function({ menuItem, order, setOrder, index}){
                 [menuItem.name]: order[menuItem.name] + 1
             }))
         }
+    }
+    function handleItemQuantityChange(event) {
+        // Get the value from the input
+        const newValue = event.target.value;
+        if(parseInt(newValue) < 1){
+            console.log(parseInt(newValue))
+            console.log('lessthanone')
+            setItemQuantity(0)
+            setItemTotal(0*menuItem.price)
+        }else{
+            setItemQuantity(newValue)
+            setItemTotal(newValue*menuItem.price)
+        }
+        
     }
     function showItemForm(event){
         setItemFormShown(true)
@@ -57,15 +73,21 @@ const OrderItem = function({ menuItem, order, setOrder, index}){
             <div className="single_item_container">
                 <div className="item_name_and_price"><span>{menuItem.name}</span> <span className="order_item_price">${menuItem.price}</span></div>
                 <p>{`${menuItem.description}`}</p>
-                {/* <button onClick={addItem}>Add to cart</button>
-                <p>In your cart: {order[menuItem.name]? order[menuItem.name]: 0}</p> */}
             </div>
             {itemFormShown? (
-                <div className="item_form_overlay" onClick={closeItemForm}>
-                    <div className={`${index%2 === 0 ? 'item_form_left' : 'item_form_right'} item_form`} style = {{top: itemFormPosition.top, left: itemFormXPosition}} onClick={handleItemFormClick}>
-                   
+            <div className="item_form_overlay" onClick={closeItemForm}>
+                <div className={`${index%2 === 0 ? 'item_form_left' : 'item_form_right'} item_form`} style = {{top: itemFormPosition.top, left: itemFormXPosition}} onClick={handleItemFormClick}> 
+                    <p>{menuItem.description}</p> 
+                    <p>Special Instructions</p>
+                    <textarea type="text" rows='2'></textarea><br/>
+                    <p>Quantity</p>
+                    <input type="number" defaultValue={itemQuantity} min={1} onChange={handleItemQuantityChange} pattern="^[1-9]\d*$"></input>
+                    <div className="add_to_cart_button">
+                        <span>${itemTotal}</span>
+                        <span>Add To Cart</span>
                     </div>
                 </div>
+            </div>
             ) : <></>}
         </div>
        
