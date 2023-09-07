1f6eadcb081ca71a388f148502a89fa1a5ef9c4a
michael seaman <mseaman26@gmail.com>
2023-09-07 12:00:04 -0700

    trying to deploy to vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(
   {
   plugins: [react()],
-  base: '/'
+  build: {
+    rollupOptions: {
+      external: ['react/jsx-runtime'],
+    },
+  },
   }
 )
