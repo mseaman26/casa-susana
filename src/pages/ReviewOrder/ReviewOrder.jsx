0f5a6a45cec154b98b8d068534993ae661088a51
michael seaman <mseaman26@gmail.com>
2023-09-13 00:40:30 -0700

    functionality for review order pages

--- src/pages/ReviewOrder/ReviewOrder.jsx
+++ src/pages/ReviewOrder/ReviewOrder.jsx
@@ -33,7 +33,7 @@ const ReviewOrder = function ({ order, setOrder, menuData }){
         console.log(itemName)
         const updatedOrder = {...order}
         delete updatedOrder[itemName]
-        setOrder2(updatedOrder)
+        setOrder(updatedOrder)
     }
 
     const getPrices = (itemName, quantity) =>{
@@ -78,20 +78,40 @@ const ReviewOrder = function ({ order, setOrder, menuData }){
             <div className="review_order_body"> 
                 <div className="check_location_review">{checkLocationText}</div>
                 <div className="not_taking_orders">{notTakingOrdersTextTop}<br/><br/>{notTakingOrdersTextBottom}<a href={`tel:18312479385`}>1-(831)-247-9385</a></div>
-                
-                {orderArray.map((orderItem, index) => {
-                    let prices = getPrices(orderItem.name, orderItem.quantity )
-                    return(
-                        // <div key={index} className="review_item">
-                        //     <h3>{orderItem.name}: Quantity: {orderItem.quantity}, Price: {prices[0]}, Item Total: {prices[1]}</h3>
-                        //     <button onClick={() => removeOne(orderItem.name)}>-</button>
-                        //     <button onClick={() => addOne(orderItem.name)}>+</button>
-                        //     <button onClick={() => clearItem(orderItem.name)}>Clear Item From Order</button>
-                        // </div>
-                        <></>
-                    )
-                })}
-       
+                <div className="order_review_container">
+                    <div className="order_review_head">
+                        <p>QTY</p>
+                        <p>Item</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="order_review_body">
+                        {orderArray.map((orderItem, index) => {
+                            let prices = getPrices(orderItem.name, orderItem.quantity )
+                            return(
+                                <>
+                                    <p>{orderItem.quantity}</p>
+                                    <p>{orderItem.name}</p>
+                                    <div className="price_and_remove_button">
+                                    <p>{prices[1]}</p><button onClick={() => clearItem(orderItem.name)}>X</button>
+                                    </div>
+                                    {/* <div key={index} className="review_item">
+                                        <h3>{orderItem.name}: Quantity: {orderItem.quantity}, Price: {prices[0]}, Item Total: {prices[1]}</h3>
+                                    
+                                        <button onClick={() => clearItem(orderItem.name)}>Clear Item From Order</button>
+                                    </div> */}
+                                </>
+                            )
+                        })}
+                    </div>
+                    <div className="order_review_totals">
+                        <p>Sub-Total</p>
+                        <p>{subTotal}</p>
+                        <p>Tax</p>
+                        <p>{tax}</p>
+                        <h3>Total</h3>
+                        <h3>{GrandTotal}</h3>
+                    </div>
+                </div>
             </div>
             <div className="revew_order_footer">
                 <div className="place_pickup_order">
